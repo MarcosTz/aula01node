4e43731e75b14b73c49bc8c68ddf2ebe5dbd1da8
MARCOS THOMAZ ROSA <marcos.trosa@senacsp.edu.br>
2023-11-08 15:00:12 -0300

    aula01 node

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import validator from 'validator' 
+
+console.log(validator.isEmail('marcostzrosa@gmail.com'))
+
+let nome: string ="joão"   
+let idade: number = 18
+
+console.log(`meu nome é ${nome}e tenho ${idade} anos`)
